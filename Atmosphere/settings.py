b2043682cdfad8fa52e879039b0ce77660a2377f
fenixsarov <fenixsarov@gmail.com>
2017-04-05 21:47:28 +0300

    Добавлена настройка для использования MySQL

--- Atmosphere/settings.py
+++ Atmosphere/settings.py
@@ -84,6 +84,12 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'OPTIONS': {
+    #         'read_default_file': os.path.join(BASE_DIR, 'my.cnf')
+    #     }
+    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Atmosphere',
