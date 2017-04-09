192a7eecca51452d9f6e7511f65ee611970ca0ef
Dmitry Tishkin <tixon@MacBook-Pro-Dmitry.local>
2017-04-09 20:54:58 +0300

    minor update

--- Atmosphere/settings.py
+++ Atmosphere/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'Atmosphere.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf')
-        }
-    },
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'OPTIONS': {
+    #         'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf')
+    #     }
+    # },
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #     'NAME': 'Atmosphere',
