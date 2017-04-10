7ff257f914f5f372693569dc55f7aebc8e43735f
Dmitry Tishkin <tixon@MacBook-Pro-Dmitry.local>
2017-04-10 22:01:52 +0300

    Сделал кое какую анимацию... Ну в общем пока мне нравитя как получается, но нужно больше анимации красивой и прикольной. Нужно подумать

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,38 @@
 $(function () {
     $('#portfolio-carousel .carousel-inner').height($(window).height());
+
+    $('#portfolio-carousel')
+        .find('.caption')
+        .animate({
+            'left': '4em',
+            'opacity': 1
+        }, 500, function () {
+            $(this).find('.description')
+                .animate({
+                    'opacity': 1
+                }, 500)
+        });
+    // Carousel
+    $('#portfolio-carousel').on('slide.bs.carousel', function () {
+        $(this).find('.caption')
+            .animate({
+                'opacity': 0,
+                'left': '50%'
+            }, 200, function () {
+                $(this).find('.description')
+                    .css('opacity', '0');
+            });
+    });
+    $('#portfolio-carousel').on('slid.bs.carousel', function () {
+        $(this).find('.caption')
+            .animate({
+                'left': '4em',
+                'opacity': 1
+            }, 500, function () {
+                $(this).find('.description')
+                    .animate({
+                        'opacity': 1
+                    }, 500)
+            });
+    });
 });
