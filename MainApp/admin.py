18d4fb8634778de59d25aae7e09146567b10abdb
fenixsarov <fenixsarov@gmail.com>
2017-06-17 20:52:16 +0300

    Fix problems in all models

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import *
+from .models import Person, Category, Privilege, Avatar, Tag, Photo, News, \
+    Request, Location, Order, Portfolio
+
 
 admin.site.register(Person)
 admin.site.register(Category)
 admin.site.register(Privilege)
+admin.site.register(Avatar)
+admin.site.register(Tag)
+admin.site.register(Photo)
+admin.site.register(News)
+admin.site.register(Request)
+admin.site.register(Location)
+admin.site.register(Order)
+admin.site.register(Portfolio)
+
 
 
 # Register your models here.
--- MainApp/models.py
+++ MainApp/models.py
@@ -6,12 +6,18 @@ class Privilege(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='Имя', max_length=15, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='Имя', max_length=15, unique=True)
     description = models.TextField(verbose_name='Описание')
-    id_privileges = models.ForeignKey(Privilege, verbose_name='Привилегии')
+    id_privileges = models.ForeignKey(Privilege)
+
+    def __str__(self):
+        return self.name
 
 
 class Person(models.Model):
@@ -24,13 +30,16 @@ class Person(models.Model):
     phone = models.CharField(verbose_name='м.телефон', max_length=11)
 
     password = models.CharField(verbose_name='Пароль', max_length=30)
-    id_category = models.ForeignKey(Category, verbose_name='Категория пользователя')
+    id_category = models.ForeignKey(Category)
 
+    def __str__(self):
+        return self.name
 
 class Avatar(models.Model):
     id = models.AutoField(primary_key=True)
     avatar_img = models.ImageField()
-    id_person = models.ForeignKey(Person, 'Пользователь')
+    id_person = models.ForeignKey(Person)
+
 
 
 # Secondary tables
@@ -38,13 +47,16 @@ class Tag(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name='Наименование', max_length=15)
 
+    def __str__(self):
+        return self.name
+
 
 class Photo(models.Model):
     id = models.AutoField(primary_key=True)
     image = models.ImageField(verbose_name='Фото')
     name = models.CharField(verbose_name='Наменование/подпись', max_length=20)
-    id_person = models.ForeignKey(Person, verbose_name='Фотограф')
-    id_tags = models.ForeignKey(Tag, verbose_name='Тэг')
+    id_person = models.ForeignKey(Person)
+    id_tags = models.ForeignKey(Tag)
 
 
 # Past-secondary tables
@@ -52,8 +64,8 @@ class News(models.Model):
     id = models.AutoField(primary_key=True)
     message = models.TextField(verbose_name='Текст новости')
     date = models.DateField(verbose_name='Дата')
-    id_newsmaker = models.ForeignKey(Person, verbose_name='Автор')
-    id_tags = models.ForeignKey(Tag, verbose_name='Тэг')
+    id_newsmaker = models.ForeignKey(Person)
+    id_tags = models.ForeignKey(Tag)
 
 
 class Request(models.Model):
@@ -63,12 +75,18 @@ class Request(models.Model):
     email = models.EmailField(verbose_name='e-mail')
     phone = models.CharField(verbose_name='Телефон', max_length=11)
 
+    def __str__(self):
+        return self.id
+
 
 class Location(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.TextField(verbose_name='Описание локации')
     photo_header = models.ImageField(verbose_name='Фото для заголовка страницы')
-    id_photo = models.ForeignKey(Photo, verbose_name='Фото')
+    id_photo = models.ForeignKey(Photo)
+
+    def __str__(self):
+        return self.id
 
 
 class Order(models.Model):
@@ -79,14 +97,17 @@ class Order(models.Model):
     # busy = models.DateTimeField(verbose_name='Занятость')
     price = models.DecimalField(verbose_name='Стоимость', max_digits=6, decimal_places=2, default=0.0)
     wish_list = models.TextField(verbose_name='Пожелания заказчика')
-    id_user = models.ForeignKey(Person, verbose_name='Фотограф')
-    id_request = models.ForeignKey(Request, verbose_name='Заявка')
+    id_user = models.ForeignKey(Person)
+    id_request = models.ForeignKey(Request)
 
 
 class Portfolio(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.TextField(verbose_name='О фотографе')
-    id_person = models.ForeignKey(Person, verbose_name='Фотограф')
-    id_photos = models.ForeignKey(Photo, verbose_name='Фото')
+    id_person = models.ForeignKey(Person)
+    id_photos = models.ForeignKey(Photo)
+
+    def __str__(self):
+        return self.id
 
 # Create your models here.
