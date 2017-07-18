8b5d7e8e6b6f9921384ce92f42738f45703e6623
fenixsarov <fenixsarov@gmail.com>
2017-07-18 15:35:59 +0300

    Fix views.py for MainApp

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render
 
-debug = True
+debug_flag = True
+
 
 def main(request):
+    debug = debug_flag
     page = 'index'
-    return render(request, 'index.html', {"page": page, "debug": debug})
+    return render(request, 'index.html', locals())
+
 
 def about(request):
+    debug = debug_flag
     page = 'about'
-    return render(request, 'about.html', {"page": page, "debug": debug})
+    return render(request, 'about.html', locals())
+
 
 def photograph(request):
+    debug = debug_flag
     page = 'photograph'
-    return render(request, 'photograph.html', {"page": page, "debug": debug})
+    return render(request, 'photograph.html', locals())
+
 
 def photographs(request):
+    debug = debug_flag
     page = 'photographs'
     # Временно отрисовывается персональная страница фотографа
-    return render(request, 'photograph.html', {"page": page, "debug": debug})
+    return render(request, 'photograph.html', locals())
+
 
 def contacts(request):
+    debug = debug_flag
     page = 'contacts'
-    return render(request, 'contacts.html', {"page": page, "debug": debug})
+    return render(request, 'contacts.html', locals())
+
 
 def galery(request):
+    debug = debug_flag
     page = 'galery'
-    return render(request, 'galery.html', {"page": page, "debug": debug})
+    return render(request, 'galery.html', locals())
 
 # Create your views here.
