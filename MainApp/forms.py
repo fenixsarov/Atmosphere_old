863497a8a085f58ed4539677f64eba96a038a8ea
fenixsarov <fenixsarov@gmail.com>
2017-07-25 04:24:16 +0300

    Added bootstrap make-up for request page

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,26 +1,30 @@
 from django import forms
 from django.forms import ModelForm
 from .models import *
+from datetime import datetime
+
+
+now = datetime.now().strftime('%d.%m.%Y')
 
 
 class RequestForm(ModelForm):
     name = forms.CharField(max_length=20,
                            label='Имя',
-                           widget=forms.TextInput(attrs={'placeholder': 'Василий'})
+                           widget=forms.TextInput(attrs={'placeholder': 'Василий', 'class': 'form-control'})
                            )
     surname = forms.CharField(max_length=20,
                               label='Фамилия',
-                              widget=forms.TextInput(attrs={'placeholder': 'Пупкин'})
+                              widget=forms.TextInput(attrs={'placeholder': 'Пупкин', 'class': 'form-control'})
                               )
     email = forms.EmailField(label='email',
-                             widget=forms.EmailInput(attrs={'placeholder': 'pupkin@mail.ru'})
+                             widget=forms.EmailInput(attrs={'placeholder': 'pupkin@mail.ru', 'class': 'form-control'})
                              )
     phone = forms.CharField(max_length=12,
                             label='Телефон',
-                            widget=forms.TextInput(attrs={'placeholder': '+79001234567'})
+                            widget=forms.TextInput(attrs={'placeholder': '+79001234567', 'class': 'form-control'})
                             )
     date = forms.DateField(label='Желаемая дата',
-                           # widget=forms.DateInput(),
+                           widget=forms.DateInput(attrs={'placeholder': now, 'class': 'form-control'}),
                            input_formats=['%d.%m.%Y']
                            )
 
